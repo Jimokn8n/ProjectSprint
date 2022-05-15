b8dfe0104717ef9a33dc8498b65f79ff29109109
Jimokn8n <gkrry7@gmail.com>
2022-05-16 04:25:56 +0900

    ProjectSprint 첫 업로드
    22.05.16 첫 업로드

---
+++ Assets/01 Script/AnimationAndMovementController.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimationAndMovementController : MonoBehaviour
+{
+
+    PlayerInput playerInput;
+    CharacterController characterController;
+    Animator anim;
+    public float movementSpeedValue;
+
+    Vector2 currentMovementInput;
+    Vector3 currentMovement;
+    bool isMovementPressed;
+    float rotationFactorPerFrame = 15.0f;
+
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+        characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+
+        playerInput.CharacterControls.Move.started += onMovementInput;
+        playerInput.CharacterControls.Move.canceled += onMovementInput;
+        playerInput.CharacterControls.Move.performed += onMovementInput;
+
+    }
+
+    void handleRotation()
+    {
+        Vector3 positionToLookAt;
+
+        positionToLookAt.x = currentMovement.x;
+        positionToLookAt.y = 0.0f;
+        positionToLookAt.z = currentMovement.z;
+        Quaternion currentRotation = transform.rotation;
+
+        if (isMovementPressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+        }
+
+    }
+
+    void onMovementInput(InputAction.CallbackContext context)
+    {
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x * movementSpeedValue;
+        currentMovement.z = currentMovementInput.y * movementSpeedValue;
+        isMovementPressed = currentMovementInput.x != 0 | currentMovementInput.y != 0;
+    }
+
+    void handleAnimation()
+    {
+        //bool isWaling;
+        bool isRunning = anim.GetBool("isRunning");
+
+        if(isMovementPressed && !isRunning)
+        {
+            anim.SetBool("isRunning", true);
+        }
+
+        else if (!isMovementPressed && isRunning)
+        {
+            anim.SetBool("isRunning", false);
+
+        }
+    }
+
+    void handleGravity()
+    {
+        if (characterController.isGrounded)
+        {
+            float groundGravity = -.05f;
+            currentMovement.y = groundGravity;
+        }
+
+        else
+        {
+            float gravity = -9.8f;
+            currentMovement.y += gravity;
+
+        }
+    }
+
+
+    void Update()
+    {
+        handleAnimation();
+        handleRotation();
+        characterController.Move(currentMovement*Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        playerInput.CharacterControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.CharacterControls.Disable();
+    }
+}
---
+++ Assets/External Assests/Crowd_Simulation_Solution/AIController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour {
+
+    
+    public GameObject goal;
+    NavMeshAgent agent;
+    Animator anim;
+
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Start() {
+        
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.transform.position);
+
+        goal = GameObject.FindGameObjectWithTag("Goal");
+        anim.SetBool("isRunning",true);
+
+        SetSpeedValueToRandom();
+    }
+
+    void SetSpeedValueToRandom()
+    {
+        agent.speed = Random.Range(6,8.2f);
+    }
+}
