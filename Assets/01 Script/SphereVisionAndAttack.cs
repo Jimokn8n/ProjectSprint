0ce8dc41606a871713d47ddbfe087f6eae10247d
Jimokn8n <92213945+Jimokn8n@users.noreply.github.com>
2022-05-24 20:20:15 +0900

    Runner 탐지 후 자연스러운 발사 구현

---
+++ Assets/01 Script/SphereVisionAndAttack.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereVisionAndAttack : MonoBehaviour
+{
+    public float range = 0f;
+    public LayerMask layerMask = 0;
+
+    Transform target = null;
+    public float spinSpeed = 0f;
+    Animator anim;
+
+    public float fireRate;
+    float currentFireRate;
+
+    public GameObject projectile;
+    public Transform muzzlePoint;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        currentFireRate = fireRate;
+        InvokeRepeating("SearchEnemy", 0f, .5f);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectAndShootEnemy();
+    }
+
+    void SearchEnemy()
+    {
+        Collider[] cols = Physics.OverlapSphere(transform.position, range, layerMask);
+        Transform shortestTarget = null;
+        if(cols.Length > 0)
+        {
+            float shortestDistance = Mathf.Infinity;
+            foreach(Collider target in cols)
+            {
+                float distance = Vector3.SqrMagnitude(transform.position - target.transform.position);
+                if(shortestDistance > distance) {
+
+                    shortestDistance = distance;
+                    shortestTarget = target.transform;
+                }
+            }
+        }
+
+        target = shortestTarget;
+    }
+
+    void DetectAndShootEnemy()
+    {
+        if (target != null)
+        {
+            //anim.SetBool("isAiming", true);
+
+            Quaternion lookRotation = Quaternion.LookRotation(target.position - transform.position);
+            Vector3 euler = Quaternion.RotateTowards(transform.rotation, lookRotation, spinSpeed * Time.deltaTime).eulerAngles;
+            transform.rotation = Quaternion.Euler(0, euler.y, 0);
+
+            Quaternion fireRotation = Quaternion.Euler(0, lookRotation.eulerAngles.y, 0);
+            if (Quaternion.Angle(transform.rotation, fireRotation) < 5f)
+            {
+                currentFireRate -= Time.deltaTime;
+                if (currentFireRate <= 0)
+                {
+                    currentFireRate = fireRate;
+                    Debug.Log("น฿ป็");
+                    anim.SetTrigger("doShoot");
+                    Rigidbody rb = Instantiate(projectile, muzzlePoint.position, Quaternion.identity).GetComponent<Rigidbody>();
+
+                    rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+                    rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+                }
+            }
+        }
+
+        else
+        {
+            //anim.SetBool("isAiming", false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+    }
